82e7843a431510dad6551b2c6e2766e7290fbc5a
Gegy Faskal Nur Hamdani <gegyhamdani@gmail.com>
2023-12-06 22:21:29 +0700

    add comment

--- src/app/services/productList.js
+++ src/app/services/productList.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// Define a service using a base URL and expected endpoints
 export const productListApi = createApi({
   reducerPath: 'fakeStoreApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com/' }),
@@ -10,4 +11,5 @@ export const productListApi = createApi({
   }),
 });
 
+// Export hooks for usage in functional components, which are
 export const { useGetProductsQuery } = productListApi;
--- src/app/store.js
+++ src/app/store.js
@@ -3,10 +3,12 @@ import { productListApi } from "./services/productList";
 import cartReducer from "../features/cart/cartSlice";
 
 export default configureStore({
+  // Adding the reducer list to the store
   reducer: {
     [productListApi.reducerPath]: productListApi.reducer,
     cart: cartReducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productListApi.middleware),
 });
