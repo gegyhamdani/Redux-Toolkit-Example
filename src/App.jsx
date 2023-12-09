a79eb23cd927ec4a61daa7930dc6293b4a7ad622
Gegy Faskal Nur Hamdani <gegyhamdani@gmail.com>
2023-12-09 21:40:56 +0700

    simplify design

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   };
 
   const handleHideCart = () => {
-    console.log(1)
+    console.log(1);
     setIsShowCart(false);
   };
 
@@ -22,12 +22,8 @@ const App = () => {
       <div className="min-h-full">
         <Header onShowCart={handleShowCart} />
 
-        <main className="-mt-32">
-          <div className="mx-auto max-w-7xl pb-12 sm:px-6">
-            <div className="bg-sky-50 h-[calc(100vh-(116px+64px+3rem))] overflow-auto rounded-lg shadow-lg border">
-              <ProductList />
-            </div>
-          </div>
+        <main className="mx-auto max-w-7xl pb-12 sm:px-6">
+          <ProductList />
         </main>
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,47 +6,35 @@ const Header = ({ onShowCart }) => {
   const totalItems = useSelector(selectCartTotalItems);
 
   const handleShowCart = () => {
-    if (totalItems === 0) return
+    if (totalItems === 0) return;
 
-    onShowCart()
-  }
-  
+    onShowCart();
+  };
+
   return (
-    <div className="bg-blue-700 pb-32">
-      <nav>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <img
-              className="h-10 w-10"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=100"
-              alt="Your Company"
-            />
-            <div className="flex items-center">
-              <button
-                type="button"
-                className="relative rounded-full bg-blue-800 p-2 text-gray-300 hover:text-white"
-                onClick={handleShowCart}
-              >
-                {totalItems ? (
-                  <span className="absolute rounded-full w-6 h-6 bg-red-600 text-sm -top-2 -right-2 flex items-center justify-center">
-                    {totalItems}
-                  </span>
-                ) : null}
-                <ShoppingCartIcon className="h-7 w-7" aria-hidden="true" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <header className="text-white">
-        <div className="mx-auto max-w-7xl px-4 pt-6 pb-14 sm:px-6 lg:px-8">
+    <header className="bg-blue-700">
+      <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-20">
           <h1 className="text-3xl font-bold tracking-tight text-gray-100">
             Product List
           </h1>
+          <div className="flex items-center">
+            <button
+              type="button"
+              className="relative rounded-full bg-blue-800 p-2 text-gray-300 hover:text-white"
+              onClick={handleShowCart}
+            >
+              {totalItems ? (
+                <span className="absolute rounded-full w-6 h-6 bg-red-600 text-sm -top-2 -right-2 flex items-center justify-center">
+                  {totalItems}
+                </span>
+              ) : null}
+              <ShoppingCartIcon className="h-7 w-7" aria-hidden="true" />
+            </button>
+          </div>
         </div>
-      </header>
-    </div>
+      </nav>
+    </header>
   );
 };
 
